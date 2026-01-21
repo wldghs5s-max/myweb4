6c1f3c465c633be7a94759633c91006aed1f9edf
DESKTOP-7BF8T50\user1 <wldghs5s@gmail.com>
2026-01-21 18:19:45 +0900

    책관리 카드게임

---
+++ book/book/book.js
@@ -0,0 +1,108 @@
+function insertBook(){
+    const title = document.querySelector("input[name=title]").value;
+    const price = document.querySelector("input[name=price]").value;
+    
+    if(!title||!price){
+        alert("입력오류");
+        return;
+    }
+    const vo = {title,price};
+
+    let arr = JSON.parse(localStorage.getItem("BookVoList"));
+    console.log(arr);
+    
+    if(!arr){arr=[];}
+    console.log(arr);
+    arr.push(vo);
+
+    localStorage.setItem("BookVoList", JSON.stringify(arr));
+    alert("저장완료!");
+    document.querySelector("input[name=title]").value = "";
+    document.querySelector("input[name=price]").value = "";
+    selectList();
+}
+
+window.onload = function(){
+    selectList();
+}
+
+function selectList(){
+    const arr = JSON.parse(localStorage.getItem("BookVoList"));
+
+    let str ="";
+    for(let i=0; i<arr.length; ++i){
+        str += `
+            <tr>
+                <td>${arr[i].title}</td>
+                <td>${arr[i].price}</td>
+                <td><input type="checkbox" value="${i+1}"></td>
+            </tr>                
+        `
+    }
+    document.querySelector("#bookList").innerHTML = str;
+}
+
+function deleteBook(){
+    const trTagList = document.querySelector("#bookList").children;
+    const targetNoArr = [];
+    console.log(trTagList);
+        for(const trTag of trTagList){
+        const inputTag = trTag.children[2].children[0];
+        if(inputTag.checked){
+            targetNoArr.push(inputTag.value);
+        }
+    }
+    targetNoArr.reverse();
+    const BookVoList = JSON.parse(localStorage.getItem("BookVoList"));
+    for(no of targetNoArr){
+        BookVoList.splice(no-1,1);
+    }
+    localStorage.setItem("BookVoList",JSON.stringify(BookVoList));
+    selectList();
+
+}
+
+function updateBook(){
+    //수정값 받아오기
+    const title = document.querySelector("input[name=upTitle]").value;
+    const price = document.querySelector("input[name=upPrice]").value;
+    //입력오류체크
+    if(!title&&!price){
+        alert("입력오류");
+        return;
+    }
+    const vo = {title,price};
+    //수정 체크 위치 받아오기
+    const trTagList = document.querySelector("#bookList").children;
+    const targetNoArr = [];
+        for(const trTag of trTagList){
+        const inputTag = trTag.children[2].children[0];
+        if(inputTag.checked){
+            targetNoArr.push(inputTag.value);
+        }
+    }
+    if(targetNoArr.length>1){
+        alert("한번에 하나씩만 수정가능");
+        return;
+    }
+    //로컬데이타 받아오기
+    const bookVoList = JSON.parse(localStorage.getItem("BookVoList"));
+    if(title){
+        bookVoList[targetNoArr[0]-1].title = title;
+    }
+    if(price){
+        bookVoList[targetNoArr[0]-1].price = price;
+    }
+    
+
+    localStorage.setItem("BookVoList", JSON.stringify(bookVoList));
+    document.querySelector("input[name=upTitle]").value = "";
+    document.querySelector("input[name=upPrice]").value = "";
+    
+    alert("수정완료!");
+
+    selectList();
+}
+
+
+
---
+++ book/cardgame/card.js
@@ -0,0 +1,112 @@
+let selectedCard01 = null;
+let selectedCard02 = null;
+let v01;
+let v02;
+
+function setListenerToCard(){
+
+    const cardAreaArr = document.querySelectorAll(".card-area");
+    
+    for(const cardArea of cardAreaArr){
+        cardArea.addEventListener("click" , function(evt){
+            //같은카드 방지
+            if(selectedCard01!==null&&!selectedCard02===null&&cardArea===selectedCard01){
+                return;
+            }
+
+            //선택된카드, 선택값 설정
+            if(selectedCard01===null && selectedCard02===null){
+                selectedCard01 = cardArea;
+                v01 = selectedCard01.children[0].children[0].innerHTML;
+            }else if(selectedCard01!==null && selectedCard02===null){
+                selectedCard02 = cardArea;
+                v02 = selectedCard02.children[0].children[0].innerHTML;
+            }
+            //카드 뒤집어서 숫자할당된 이미지 보여주기
+            const temp = evt.currentTarget;
+            temp.classList.toggle("flip");
+            
+            //1번카드, 2번카드 채워져있으면 다시 뒤집기
+            if(selectedCard01!==null&&selectedCard02!==null){
+                const x = selectedCard01;
+                const y = selectedCard02;
+                setTimeout(()=>{
+                    x.classList.remove("flip");
+                    y.classList.remove("flip");
+                },1000
+                )
+            }
+            //값 일치하면 숨기기
+            if(v01!==null&&v02!==null&&v01===v02){
+                const removeCard01 = selectedCard01;
+                const removeCard02 = selectedCard02;
+                //순차적으로 사라지는현상 때문에 하위자식들도 전부 하이드클래스 적용
+                setTimeout(function(){
+                    removeCard01.classList.add("hide");
+                    removeCard01.children[0].children[0].classList.add("hide");
+                    removeCard01.children[0].children[1].classList.add("hide");
+                    removeCard01.children[0].classList.add("hide");
+                    removeCard02.classList.add("hide");
+                    removeCard02.children[0].children[0].classList.add("hide");
+                    removeCard02.children[0].children[1].classList.add("hide");
+                    removeCard02.children[0].classList.add("hide");
+
+                },1000)
+            }
+            //카드 두장 골랐으면, 변수 초기화 하기
+            if(selectedCard01!==null&&selectedCard02!==null&&v01!==null&&v02!==null){
+                selectedCard01 = null;
+                selectedCard02 = null;
+                v01 = null;
+                v02 = null;
+            }
+      }
+        ) 
+    }
+}
+
+let ma = document.querySelector("#ma");
+
+function generateCardList(){
+    const cardCnt = document.querySelector("#cardCnt").value;
+    if(cardCnt > 30){
+        alert("최대 30개 까지 생성가능")
+        return;
+    }
+    ma.innerHTML = "";
+
+    const cardContentArr = [];
+    for(let i = 1; i<= cardCnt; i++){
+        cardContentArr.push(i);
+    }
+    const arr = cardContentArr.concat(cardContentArr);
+
+    const result = shuffleArr(arr);
+    for(temp of arr){
+        ma.innerHTML += `
+            <div class="card-area">
+                    <div class="card">
+                    <div class="card-back"><img src="../static/${temp}.jpg" alt="" width="80px" height="108px"></div>
+                    <div class="card-front"><img src="../static/front.jpg" alt="" width="80px" height="108px"></div>
+                </div>
+            </div>
+`;
+    }
+}
+
+function shuffleArr(arr){
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  return arr;
+}
+
+function handleClick(){
+    generateCardList();
+    setListenerToCard();
+}
+
+
+
+
